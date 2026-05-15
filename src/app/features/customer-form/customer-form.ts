40e151357a75972362ccd9a7199ea49e29d14c8a
zookat <zotova.ea@gmail.com>
2026-05-15 13:30:50 +0200

    feat: refactor customer form to use FormBuilder and Validators

--- src/app/features/customer-form/customer-form.ts
+++ src/app/features/customer-form/customer-form.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-customer-form',
   imports: [ReactiveFormsModule],
   templateUrl: './customer-form.html',
   styleUrl: './customer-form.css',
 })
 export class CustomerForm {
-  form = new FormGroup({
-    fullName: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    address: new FormControl(''),
-    loyalty: new FormControl(''),
-    state: new FormControl(''),
-    image: new FormControl(''),
+  private formBuilder = inject(FormBuilder);
+  form = this.formBuilder.group({
+    fullName: ['', Validators.required],
+    email: ['', Validators.required],
+    phone: ['', Validators.required],
+    address: ['', Validators.required],
+    loyalty: ['', Validators.required],
+    state: ['', Validators.required],
+    image: ['', Validators.required],
   });
 
   onSubmit() {
