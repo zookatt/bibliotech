1a8dc203908f23966d4802f608d71148fc7520c9
rglepe <raul.garcia@factoriaf5.org>
2026-05-18 10:30:26 +0200

    fix(routes): minor adjustments after layout refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,37 @@
 import { Routes } from '@angular/router';
 import { Layout } from './pages/layout/layout';
+import { Dashboard } from './features/dashboard/dashboard';
+import { CustomerList } from './features/customer/customer-list/customer-list';
+import { CustomerSForm } from './features/customer/customer-forms/customer-s-form/customer-s-form';
 
 export const routes: Routes = [
   {
-    path:'dashboard', 
+    path: '',
     component: Layout,
-  }
+    children: [
+      {
+        path: '',
+        component: Dashboard
+      },
+
+      {
+        path: 'dashboard',
+        redirectTo: '/'
+      },
+      {
+        path: 'customers',
+        component: CustomerList,
+      },
+            {
+        path: 'customers/:customerId',
+        component: CustomerSForm,
+      },
+      {
+        path:'**',
+        component: Dashboard
+      }
+
+    ]
+  },
+
 ];
