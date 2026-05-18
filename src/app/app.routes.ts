11fb66049a5c4611bd7de382c6aa37bd4a6ed9da
rglepe <raul.garcia@factoriaf5.org>
2026-05-18 09:42:33 +0200

    fix(routes): update app.routes after customer refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Layout } from './pages/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'dashboard', 
+    component: Layout,
+  }
+];
