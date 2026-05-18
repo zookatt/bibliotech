a04aad602f0055cda7961d9cdab3fc55f7cb43b0
rglepe <raul.garcia@factoriaf5.org>
2026-05-18 10:31:44 +0200

    feat(layout): add aside component and template

---
+++ src/app/features/layout/aside/aside.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Aside } from './aside';
+
+describe('Aside', () => {
+  let component: Aside;
+  let fixture: ComponentFixture<Aside>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Aside],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Aside);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/layout/aside/aside.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside',
+  imports: [],
+  templateUrl: './aside.html',
+  styleUrl: './aside.css',
+})
+export class Aside {}
